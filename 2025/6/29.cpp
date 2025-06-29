4237223480ac40c2f286a8dbbba437933071f09c
张鑫 <xin302452@gmail.com>
2025-06-29 20:00:46 +0800

    change1

--- 2025/6/29.cpp
+++ 2025/6/29.cpp
@@ -8,13 +8,21 @@ typedef struct{
     int length;
 }SqList;
 
+void printList(SqList L)
+{
+    for(int i = 0;i < L.length;i++)
+    {
+        cout << L.elem[i] << " ";
+    }
+}
 int main(){
     SqList L;
+    L.length = 5;
     L.elem[0] = 1;
-    L.elem[0] = 2;
-    L.elem[0] = 3;
-    L.elem[0] = 4;
-    L.elem[0] = 5;
-    
+    L.elem[1] = 2;
+    L.elem[2] = 3;
+    L.elem[3] = 4;
+    L.elem[4] = 5;
+    printList(L);
     return 0;
 }
