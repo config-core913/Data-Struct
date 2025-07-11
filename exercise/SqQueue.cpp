cb3ad3f3abbae2dfd3767b95fac556aeb97d93d3
config_core.int <xin302452@gmail.com>
2025-07-11 18:33:47 +0800

    SqQueue En and De

--- exercise/SqQueue.cpp
+++ exercise/SqQueue.cpp
@@ -17,9 +17,46 @@ bool InitQueue(SqQueue &Q){
     Q.front = Q.rear = 0;
     return true;
 }
+//求队列长度
+int QueueLength(SqQueue Q){
+    return (Q.rear - Q.front + MaxSize) % MaxSize;
+}
+//入队
+bool EnQueue(SqQueue &Q,ElemType element){
+    if((Q.rear + 1) % MaxSize == Q.front){
+        return false;
+    }
+    Q.base[Q.rear] = element;
+    Q.rear = (Q.rear + 1) % MaxSize;
+    return true;
+}
+//出队
+bool DeQueue(SqQueue &Q,ElemType &e){
+    if(Q.front == Q.rear){
+        return false;
+    }
+    e = Q.base[Q.front];
+    Q.front = (Q.front + 1) % MaxSize;
+    return true;
+}
+//获取队头元素
+ElemType GetHead(SqQueue Q){
+    if(Q.front != Q.rear){
+        return Q.base[Q.front];
+    }
+}
 int main(){
     SqQueue Q;
     InitQueue(Q);
-    cout << "   \n\n" << endl;
+
+    EnQueue(Q,10);
+    EnQueue(Q,2);
+    EnQueue(Q,3);
+    EnQueue(Q,4);
+    EnQueue(Q,5);
+    EnQueue(Q,6);
+
+    ElemType test = GetHead(Q);
+    cout << test << endl;
     return 0;
 }
