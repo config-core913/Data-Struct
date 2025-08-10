9061a059605de370fba0de1b39a83800e8547541
config_core.int <xin302452@gmail.com>
2025-08-10 14:01:27 +0800

    2013年真题第二题

--- SqList/reverse.cpp
+++ SqList/reverse.cpp
@@ -7,21 +7,30 @@ typedef struct{
     int *data;
     int length;
 }SqList;
-
+//首位交换元素
+//排序算法-------
 void reverse(SqList &L,int length){
-    for(int i = 0;i < L.length/2;i++){
-        
+    for(int i = 0;i < L.length/2 ;i++){
+        int temp = L.data[i];
+        L.data[i] = L.data[L.length - 1 - i];
+        L.data[L.length - 1 - i] = temp;
     }
+    delete[] L.data;
 }
 int main(){
     SqList L;
     L.length = 0;
+    L.data = new int[10];
     cout << "降序输出" << endl;
     for(int i = 0;i < 10;i++){
         L.data[i] = 10 - i;
         cout << L.data[i] << " ";
         L.length++;
     }
-    
+    cout << endl << "修改后：" << endl;
+    reverse(L,L.length);
+    for(int i = 0;i < 10;i++){
+        cout << L.data[i] << " ";
+    }
     return 0;
 }
