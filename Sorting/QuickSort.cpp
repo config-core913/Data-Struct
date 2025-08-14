d1f7d0e7fe28b0b6cf40544fe1b83045fdc00711
config_core.int <xin302452@gmail.com>
2025-08-14 14:09:43 +0800

    快速排序

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -11,6 +11,7 @@ typedef struct {
     RedType r[MAXSIZE];
     int length;
 }SqList;
+//对序列 L 的 [low, high] 区间进行一趟划分
 int Partition(SqList &L,int low,int high){
     L.r[0] = L.r[low];
     int pivotkey = L.r[low].key;
@@ -27,14 +28,19 @@ int Partition(SqList &L,int low,int high){
     L.r[low] = L.r[0];
     return low; //返回枢纽位置
 }
+//对序列 L 的 [low, high] 区间进行递归快速排序
 void QSort(SqList &L,int low,int high){
-    if(low < high){
+    // 递归的基准情况：如果区间为空或只有一个元素，则直接返回
+    if(low < high){ // 调用 Partition 函数，对当前区间进行划分
+                    // pivotloc 是枢纽元素在本轮排序后尘埃落定的位置
         int pivotloc = Partition(L,low,high);
-        QSort(L,low,pivotloc - 1);
-        QSort(L,pivotloc + 1,high);
+        QSort(L,low,pivotloc - 1);  //左边的子序列
+        QSort(L,pivotloc + 1,high); //右边的子序列
     }
 }
+//快速排序的启动函数
 void QuickSort(SqList &L){
+    // 从序列的第一个元素到最后一个元素，启动递归快速排序
     QSort(L,1,L.length);
 }
 int main(){
