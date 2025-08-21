97f66c8bea79f540110babc342b28c9273f26b05
config_core.int <xin302452@gmail.com>
2025-08-21 14:44:00 +0800

    2016年第一个

---
+++ SqList/2016.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+//2016年第一大题
+//顺序表逆置
+void reverse(int A[],int begin,int end){
+    int temp;
+    int j = end - 1;
+    int i = begin;
+    while(i < j){
+        temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+        i++;
+        if(i == j) break;
+        j--;
+    }
+}
+
+void rotate(int A[],int length,int k){
+    reverse(A,0,length);
+    reverse(A,0,length - k);
+    reverse(A,length - k,length);
+}
+int main(){
+    int A[10] = {1,2,3,4,5,6,7,8,9,10};
+    int length = 10;
+    cout << "移动前：";
+    for(int i = 0;i < length;i++){
+        cout << A[i] << " ";
+    }
+    cout << endl;
+    
+    rotate(A,length,3);
+    cout << "移动后：";
+    for(int i = 0;i < length;i++){
+        cout << A[i] << " ";
+    }
+    return 0;
+}
