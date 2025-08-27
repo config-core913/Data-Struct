b1f9dc8e74f094fbd077296c8eb5991de804c18a
config_core.int <xin302452@gmail.com>
2025-08-27 18:44:28 +0800

    2019第一大题

---
+++ SqList/2019.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+//2019年第一大题
+//动态数组在最后插入元素，更新数组首地址
+typedef int ElemType;
+#define MAXSIZE 100	//数据总量 m 
+#define MAX 10 		//n个数组
+void Insert(ElemType D[],ElemType element,int i,){
+
+}
+int main(){
+
+	return 0;
+}
