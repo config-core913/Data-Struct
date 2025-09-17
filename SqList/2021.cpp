b0f2d9d9becf26edf7713a93777226c594e680e4
config_core.int <xin302452@gmail.com>
2025-09-17 15:26:27 +0800

    2021年第一大题

--- SqList/2021.cpp
+++ SqList/2021.cpp
@@ -1,8 +1,9 @@
 #include<iostream>
 using namespace std;
 //21年双调序列找最大值
+
 //枚举法，非高效做法
-int findMAX(int A[],int length){
+int findMAX1(int A[],int length){
 	for(int i = 1;i < length;i++){
 		if(A[i] > A[i-1] && A[i] > A[i+1]){
 			return A[i];
@@ -10,11 +11,33 @@ int findMAX(int A[],int length){
 	}
 	return 0;
 }
-//
+//二分查找法
+int findMAX2(int A[],int length){
+	if (length <= 0) {
+        return -1; // 表示输入无效
+    }
+    if (length == 1) {//只有一个元素
+        return A[0];
+    }
+	int low = 0;
+	int hight = length-1;
+	int mid;
+	while(low < hight){
+		mid = (low + hight)/2;
+		if(A[mid] > A[mid-1] && A[mid] > A[mid+1]){
+			return A[mid];
+		}else if(A[mid] > A[mid-1] && A[mid] < A[mid+1]){
+			low = mid;
+		}else{
+			hight = mid;
+		}
+	}
+	return -1;
+}
 int main(){
-	int A[] = {1,2,3,4,5,6,7,8,100,8,7,6,5,4,3,2,1,0};
+	int A[] = {1,2,3,4,5,6,7,8,100,120,7,6,5,4,3,2,1,0};
 	int length = 18;
-	int ret = findMAX(A,length);
+	int ret = findMAX2(A,length);
 	if(ret){
 		cout << "最大值为：" << ret << endl;
 	}else{
